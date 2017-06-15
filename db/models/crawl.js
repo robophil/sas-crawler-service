78c1e7f717cdbc358a425c8ca706fe52867e7876
Balogun Oghenerobo Philip <oghenerobo@gmail.com>
2017-06-15 16:21:45 +0100

    crawl model

---
+++ db/models/crawl.js
@@ -0,0 +1,47 @@
+const Waterline = require('waterline')
+const config = require('../config')
+
+// Define your collection (aka model) 
+module.exports = Waterline.Collection.extend(Object.assign({
+
+    tableName: 'crawl',
+
+    identity: 'crawl',
+
+    attributes: {
+
+        store: {
+            type: 'objectid'
+        },
+
+        category: {
+            type: 'objectid'
+        },
+
+        subcategory: {
+            type: 'objectid'
+        },
+
+        message: {
+            type: 'string'
+        },
+
+        job: {
+            type: 'string',
+            enum: ['cron', 'human'],
+            defaultsTo: 'human'
+        },
+        
+        status: {
+            type: 'string',
+            enum: ['pending', 'running', 'done'],
+            defaultsTo: 'pending'
+        },
+
+        time: {
+            type: 'datetime',
+            required: true
+        }
+
+    }
+}, config))
